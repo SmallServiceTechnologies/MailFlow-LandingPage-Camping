36d72f81e1f9f81bf35b7703c7bb12c86c7e7256
Flamur-Maliqi <flamur.maliqi@ibm.com>
2024-12-08 10:48:20 +0100

    pictures hinzugefügt

--- MailFlow-LandingPage/src/components/MainContent/MainComponent.jsx
+++ MailFlow-LandingPage/src/components/MainContent/MainComponent.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./MainComponent.css";
 import FAQ from "../FAQ/FAQ";
 import Arrow from '../../assets/Arrow.png';  
+import cardsWithArrow from "../../assets/cardsWithArrow.png";
+import cardsWithArrowResponsive from "../../assets/cardsWithArrow-responsive.png";
 
 export default function MainComponent({ openCalendly }) {
+  const [currentImage, setCurrentImage] = useState(cardsWithArrow);
+
+  // Funktion zur Überwachung der Bildschirmgröße
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setCurrentImage(cardsWithArrowResponsive);
+      } else {
+        setCurrentImage(cardsWithArrow);
+      }
+    };
+
+    // Initiale Überprüfung und Eventlistener hinzufügen
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // Eventlistener beim Unmount entfernen
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleClick = () => {
     openCalendly();
@@ -11,58 +34,62 @@ export default function MainComponent({ openCalendly }) {
 
   return (
     <div>
-    <div className="backgroundcolor-grey">
-      <div className="arrow-container">
-      <img src={Arrow} alt="Arrow Icon" className="arrow-image" />
-      </div>
-      <div className="margin-center ">
-        <div className="main-container">
-          <div className="cta-container">
-            <h1 className="h1-header">
-              Schluss mit wiederholten E-Mails – keine Zeit mehr verschwenden.
-            </h1>
-            <ul>
-              <li>
-                <strong className= "strong">Wiederkehrende Fragen eliminieren</strong>: Automatische
-                Antworten auf häufige Anfragen.
-              </li>
-              <li>
-                <strong className= "strong">Buchungen automatisieren</strong>: Anfragen direkt mit
-                CRM-Integration bearbeiten.
-              </li>
-              <li>
-                <strong className= "strong">Gästezufriedenheit erhöhen</strong>: Schnelle und
-                präzise Antworten.
-              </li>
-              <li>
-                <strong className= "strong" >Zeit sparen</strong>: Fokus auf die wichtigen Aufgaben
-                Ihres Campingplatzes.
-              </li>
-            </ul>
-            <button onClick={handleClick} className="call-to-action">
-              Jetzt Termin vereinbaren!
-            </button>
-          </div>
-          <div className="video-container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/IiB02GFi1FU"
-              allowFullScreen
-              title="YouTube video"
-            ></iframe>
-            <button
-              onClick={handleClick}
-              className="small-screen-call-to-action"
-            >
-              Jetzt Termin vereinbaren!
-            </button>
+      <div className="backgroundcolor-grey">
+        <div className="arrow-container">
+          <img src={Arrow} alt="Arrow Icon" className="arrow-image" />
+        </div>
+        <div className="margin-center">
+          <div className="main-container">
+            <div className="cta-container">
+              <h1 className="h1-header">
+                Schluss mit wiederholten E-Mails – keine Zeit mehr verschwenden.
+              </h1>
+              <ul>
+                <li>
+                  <strong className="strong">Wiederkehrende Fragen eliminieren</strong>: Automatische Antworten auf häufige Anfragen.
+                </li>
+                <li>
+                  <strong className="strong">Buchungen automatisieren</strong>: Anfragen direkt mit CRM-Integration bearbeiten.
+                </li>
+                <li>
+                  <strong className="strong">Gästezufriedenheit erhöhen</strong>: Schnelle und präzise Antworten.
+                </li>
+                <li>
+                  <strong className="strong">Zeit sparen</strong>: Fokus auf die wichtigen Aufgaben Ihres Campingplatzes.
+                </li>
+              </ul>
+              <button onClick={handleClick} className="call-to-action">
+                Jetzt Termin vereinbaren!
+              </button>
+            </div>
+            <div className="video-container">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/IiB02GFi1FU"
+                allowFullScreen
+                title="YouTube video"
+              ></iframe>
+              <button
+                onClick={handleClick}
+                className="small-screen-call-to-action"
+              >
+                Jetzt Termin vereinbaren!
+              </button>
+            </div>
           </div>
         </div>
       </div>
+      <div className="margin-center">
+        {/* Dynamisch ausgewähltes Bild basierend auf der Bildschirmgröße */}
+        <img
+          id="cardsWithArrowImage"
+          src={currentImage}
+          alt="Cards with Arrow Icon"
+        />
       </div>
-      <div className="margin-center ">
-      <FAQ></FAQ>
+      <div className="margin-center">
+        <FAQ></FAQ>
       </div>
     </div>
   );
