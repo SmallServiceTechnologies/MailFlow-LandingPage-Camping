99a00616a1428333e5ea0002312f59616dff0fd2
Flamur-Maliqi <flamur.maliqi@ibm.com>
2024-12-10 13:21:51 +0100

    Learn more button

--- MailFlow-LandingPage/src/components/MainContent/MainComponent.jsx
+++ MailFlow-LandingPage/src/components/MainContent/MainComponent.jsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./MainComponent.css";
 import FAQ from "../FAQ/FAQ";
-import Arrow from '../../assets/Arrow.png';  
+import Arrow from "../../assets/Arrow.png";
 import cardsWithArrow from "../../assets/cardsWithArrow.png";
 import cardsWithArrowResponsive from "../../assets/cardsWithArrow-responsive.png";
 import ExplainerComponent from "../ExplainerComponent/ExplainerComponent";
 
 export default function MainComponent({ openCalendly }) {
   const [currentImage, setCurrentImage] = useState(cardsWithArrow);
+
+  // Referenz für den FAQ-Bereich
+  const faqRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => {
@@ -16,7 +19,7 @@ export default function MainComponent({ openCalendly }) {
       } else {
         setCurrentImage(cardsWithArrow);
       }
-    } ;
+    };
 
     handleResize();
     window.addEventListener("resize", handleResize);
@@ -28,6 +31,11 @@ export default function MainComponent({ openCalendly }) {
 
   const handleClick = () => {
     openCalendly();
+  };
+
+  // Funktion für das Scrollen zum FAQ-Bereich
+  const handleLearnMoreClick = () => {
+    faqRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -43,35 +51,68 @@ export default function MainComponent({ openCalendly }) {
                 Schluss mit wiederholten E-Mails – keine Zeit mehr verschwenden.
               </h1>
               <ul>
-                <li>Wiederkehrende Fragen eliminieren: Automatische Antworten auf häufige Anfragen.</li>
-                <li>Buchungen automatisieren: Anfragen direkt mit CRM-Integration bearbeiten.</li>
-                <li>Gästezufriedenheit erhöhen: Schnelle und präzise Antworten.</li>
-                <li>Zeit sparen: Fokus auf die wichtigen Aufgaben Ihres Campingplatzes.</li>
+                <li>
+                  Wiederkehrende Fragen eliminieren: Automatische Antworten auf
+                  häufige Anfragen.
+                </li>
+                <li>
+                  Buchungen automatisieren: Anfragen direkt mit CRM-Integration
+                  bearbeiten.
+                </li>
+                <li>
+                  Gästezufriedenheit erhöhen: Schnelle und präzise Antworten.
+                </li>
+                <li>
+                  Zeit sparen: Fokus auf die wichtigen Aufgaben Ihres
+                  Campingplatzes.
+                </li>
               </ul>
-              <button onClick={handleClick} className="call-to-action">Jetzt Termin vereinbaren!</button>
+              <div className="button-container">
+                <button
+                  onClick={handleClick}
+                  className="call-to-action button-effect"
+                >
+                  Jetzt Termin vereinbaren!
+                </button>
+                <button
+                  onClick={handleLearnMoreClick}
+                  className="learn-more-button button-effect"
+                >
+                  Erfahre mehr!
+                </button>
+              </div>
             </div>
             <div className="video-container">
               <iframe
                 src="https://www.youtube.com/embed/IiB02GFi1FU"
-                id = "video"
+                id="video"
                 allowFullScreen
                 title="YouTube video"
               ></iframe>
             </div>
-            <button
+            <div>
+              <button
                 onClick={handleClick}
-                className="small-screen-call-to-action"
+                className="small-screen-call-to-action button-effect"
               >
                 Jetzt Termin vereinbaren!
               </button>
+              <button
+                onClick={handleLearnMoreClick}
+                className="small-screen-learn-more button-effect"
+              >
+                Erfahre mehr!
+              </button>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="margin-center">
-       <ExplainerComponent></ExplainerComponent> 
+        <ExplainerComponent />
       </div>
-      <div className="margin-center">
-        <FAQ></FAQ>
+      <div className="margin-center" ref={faqRef}>
+        <FAQ />
       </div>
     </div>
   );
