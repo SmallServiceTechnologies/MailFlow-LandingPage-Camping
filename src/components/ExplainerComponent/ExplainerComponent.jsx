833a1472bffb022596cd3a691b95987fc95ffe80
Jure Kleindienst <jurekleindienst@Jures-MacBook-Pro.local>
2024-12-13 22:05:46 +0100

    changed graphic

--- src/components/ExplainerComponent/ExplainerComponent.jsx
+++ src/components/ExplainerComponent/ExplainerComponent.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import "./ExplainerComponent.css"
-import anfrage from "../../assets/anfrage_1.svg";
-import antwort from "../../assets/antwort_1.svg";
-import arrowQuestion from "../../assets/arrowQuestion.png";
-import arrowAnswer from "../../assets/arrowAnswer.png"
-import kunde from "../../assets/Kunde.png"
 import anfrage_email from "../../assets/anfrage_email.svg"
-import antwort_email from "../../assets/antwort_email.svg"
 import antwort_email_1 from "../../assets/antwort_email_2.svg";
 import arrow_down from "../../assets/arrow_down_1.svg";
+import anfrage_email_1 from "../../assets/anfrage_fixed.svg";
 
 
 import "./ExplainerComponent.css";
@@ -23,7 +18,7 @@ const ExplainerComponent = () => {
                         <div className="explainer-photo-text">
                             <p>Kunde stellt Anfrage</p>
                         </div>
-                        <img src={anfrage_email} alt="Mail Icon"/>
+                        <img src={anfrage_email_1} alt="Mail Icon"/>
                     </div>
                     <div className="div-block-explainer-arrow">
                         <img src={arrow_down} className="arrow-image"/>
