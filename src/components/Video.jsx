0c7a66bad6e49850bd02f81b1a50979bc84a4400
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-01-04 12:15:42 +0100

    Bug fix Google Consent Manager

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -17,7 +17,7 @@ export default function Video() {
     return (
         <div className="video-container" onClick={handleAcceptCookies}>
             <span id="video-text-overlay">
-               Mit Start des Videos stimmen Sie der Nutzung von Cookies zu. Lehnen Sie explizit Cookies ab, um das Video ohne sie abzuspielen.</span>
+               Mit Start des Videos stimmen Sie der Nutzung von Cookies zu. Lehnen Sie Cookies explizit ab, um das Video ohne sie abzuspielen.</span>
             <iframe
                 src={iframeSrc}
                 id="youtube-video"
