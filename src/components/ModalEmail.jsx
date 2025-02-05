dcfe868f72753c055aa624ef1627c10eb4e9bcef
Flamur-Maliqi <flamur.maliqi@ibm.com>
2025-02-05 08:29:00 +0100

    New Toogle options are available that are the content for the email
    Central variable is emailOptions that stores the content of every radioButton. Based on that the email is dynmically created

--- src/components/ModalEmail.jsx
+++ src/components/ModalEmail.jsx
@@ -1,26 +1,75 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from "react";
+import Accordion from "./Accordion.jsx";
 import "../css/modalEmail.css";
+
 export default function ModalEmail({ onClose }) {
-    const modalRef = useRef();
-    const closeModal = (e) => {
-        if (modalRef.current === e.target) {
-            onClose();
-        }
-    };
-    const openEmailClient = () => {
-        const email = "test@example.com";  
-        const subject = "Moritz gay";
-        const body = "Moritz gay";
-        window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    };
-    return (
-        <div ref={modalRef} onClick={closeModal} className='modal-container'>
-            <div className='modal-content'>
-                <button className="exit-button" onClick={onClose}>x</button>
-                <h1>Moritz ist gay</h1>
-                <p>Choose your favorite email</p>
-                <button onClick={openEmailClient}>Get test email</button>
+  const modalRef = useRef();
+  const [selectedId, setSelectedId] = useState(null);
+  const [emailData, setEmailData] = useState({ subject: "", body: "" });
+
+  const emailOptions = [
+    {
+      id: "verfügbarkeitsanfrage",
+      title: "Verfügbarkeitsanfrage",
+      content: "Das ist eine Verfügbarkeitsanfrage",
+    },
+    {
+      id: "buchungsanfrage",
+      title: "Buchungsanfrage",
+      content: "Das ist eine Buchungsanfrage",
+    },
+    { 
+        id: "bewerbung", 
+        title: "Bewerbung", 
+        content: "Das ist eine Bewerbung" 
+    },
+  ];
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      onClose();
+    }
+  };
+
+  const openEmailClient = () => {
+    if (!emailData.subject || !emailData.body) return;
+    const email = "demo@flow-suite.de";
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(
+      emailData.subject
+    )}&body=${encodeURIComponent(emailData.body)}`;
+  };
+
+  const handleSelection = (id, title, content) => {
+    setSelectedId(id);
+    setEmailData({ subject: title, body: content });
+  };
+
+  return (
+    <div ref={modalRef} onClick={closeModal} className="modal-container">
+      <div className="modal-content">
+        <button className="exit-button" onClick={onClose}>
+          x
+        </button>
+        <h1>Moritz ist gay</h1>
+        <p>Choose your favorite email: </p>
+        <div className="email-options">
+          {emailOptions.map(({ id, title, content }) => (
+            <div className="email-option" key={id}>
+              <input
+                type="radio"
+                name="option"
+                id={id}
+                checked={selectedId === id}
+                onChange={() => handleSelection(id, title, content)}
+              />
+              <Accordion title={title} content={content} htmlFor={id} />
             </div>
+          ))}
         </div>
-    );
+        <button onClick={openEmailClient} disabled={!emailData.subject}>
+          Get test email (Btw Moritz is gay)
+        </button>
+      </div>
+    </div>
+  );
 }
