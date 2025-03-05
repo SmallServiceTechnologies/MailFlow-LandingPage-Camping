929083ee6407a9ad49d83dee3a6f963fc0a56913
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-05 09:47:53 +0100

    WiP

---
+++ src/components/DemoEmailSelector.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../css/demoEmailSelector.css"
+
+export default function DemoEmailSelector() {
+    return (
+        <div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
