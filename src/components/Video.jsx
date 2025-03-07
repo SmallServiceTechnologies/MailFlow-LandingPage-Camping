5f4331214b2e1925924c8c18da6249e16eb2fff1
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-07 18:16:43 +0100

    Disabled preview of other videos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -11,8 +11,8 @@ export default function Video() {
 
     // Dynamically set the iframe src based on cookie consent
     const iframeSrc = cookieConsent
-        ? "https://www.youtube.com/embed/TyCqL-9zf_Q?enablejsapi=1&vq=hd1080"
-        : "https://www.youtube-nocookie.com/embed/TyCqL-9zf_Q?enablejsapi=1&vq=hd1080";
+        ? "https://www.youtube.com/embed/TyCqL-9zf_Q?enablejsapi=1&vq=hd1080&rel=0"
+        : "https://www.youtube-nocookie.com/embed/TyCqL-9zf_Q?enablejsapi=1&vq=hd1080&rel=0";
 
     return (
         <div className="video-container" onClick={handleAcceptCookies}>
